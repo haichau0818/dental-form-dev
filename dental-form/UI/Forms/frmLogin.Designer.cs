12cf7a7d24dd24afe619568c948887dd52be8fa2
haichau0818 <haichau0818@gmail.com>
2023-02-03 03:20:29 +0700

    edit login form 03/02/2023

--- dental-form/UI/Forms/frmLogin.Designer.cs
+++ dental-form/UI/Forms/frmLogin.Designer.cs
@@ -38,12 +38,13 @@
             this.picClose = new DevExpress.XtraEditors.PictureEdit();
             this.picIFormLogin = new DevExpress.XtraEditors.PictureEdit();
             this.pnlLogin = new DevExpress.XtraEditors.PanelControl();
-            this.lbl_Welcome = new DevExpress.XtraEditors.LabelControl();
-            this.btn_Login = new DevExpress.XtraEditors.SimpleButton();
+            this.pnlLoginUP = new System.Windows.Forms.Panel();
+            this.chkDahuy_Toathuoc = new DevExpress.XtraEditors.CheckEdit();
             this.txt_Password = new DevExpress.XtraEditors.TextEdit();
             this.txt_UserName = new DevExpress.XtraEditors.TextEdit();
+            this.lbl_Welcome = new DevExpress.XtraEditors.LabelControl();
+            this.btn_Login = new DevExpress.XtraEditors.SimpleButton();
             this.lbl_TitleLogin = new DevExpress.XtraEditors.LabelControl();
-            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
             ((System.ComponentModel.ISupportInitialize)(this.tpnlLogin)).BeginInit();
             this.tpnlLogin.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tpnlImgLogin)).BeginInit();
@@ -57,9 +58,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.picIFormLogin.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pnlLogin)).BeginInit();
             this.pnlLogin.SuspendLayout();
+            this.pnlLoginUP.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chkDahuy_Toathuoc.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txt_Password.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txt_UserName.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // tpnlLogin
@@ -119,7 +121,7 @@
             // 
             this.lbl_Version.Appearance.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lbl_Version.Appearance.Options.UseFont = true;
-            this.lbl_Version.Location = new System.Drawing.Point(389, 7);
+            this.lbl_Version.Location = new System.Drawing.Point(398, 9);
             this.lbl_Version.Name = "lbl_Version";
             this.lbl_Version.Size = new System.Drawing.Size(73, 17);
             this.lbl_Version.TabIndex = 0;
@@ -172,6 +174,7 @@
             this.picClose.Size = new System.Drawing.Size(42, 25);
             this.picClose.TabIndex = 0;
             this.picClose.EditValueChanged += new System.EventHandler(this.picClose_EditValueChanged);
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
             // 
             // picIFormLogin
             // 
@@ -193,11 +196,9 @@
             this.pnlLogin.Appearance.BackColor2 = System.Drawing.Color.White;
             this.pnlLogin.Appearance.Options.UseBackColor = true;
             this.tpnlLogin.SetColumn(this.pnlLogin, 0);
-            this.pnlLogin.Controls.Add(this.checkEdit1);
+            this.pnlLogin.Controls.Add(this.pnlLoginUP);
             this.pnlLogin.Controls.Add(this.lbl_Welcome);
             this.pnlLogin.Controls.Add(this.btn_Login);
-            this.pnlLogin.Controls.Add(this.txt_Password);
-            this.pnlLogin.Controls.Add(this.txt_UserName);
             this.pnlLogin.Controls.Add(this.lbl_TitleLogin);
             this.pnlLogin.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlLogin.Location = new System.Drawing.Point(3, 3);
@@ -210,34 +211,35 @@
             this.pnlLogin.Size = new System.Drawing.Size(404, 594);
             this.pnlLogin.TabIndex = 0;
             // 
-            // lbl_Welcome
+            // pnlLoginUP
             // 
-            this.lbl_Welcome.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_Welcome.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(112)))), ((int)(((byte)(148)))));
-            this.lbl_Welcome.Appearance.Options.UseFont = true;
-            this.lbl_Welcome.Appearance.Options.UseForeColor = true;
-            this.lbl_Welcome.Location = new System.Drawing.Point(37, 177);
-            this.lbl_Welcome.Name = "lbl_Welcome";
-            this.lbl_Welcome.Size = new System.Drawing.Size(256, 15);
-            this.lbl_Welcome.TabIndex = 7;
-            this.lbl_Welcome.Text = "Welcome to DENTAL CARE  login to take action!";
+            this.pnlLoginUP.BackColor = System.Drawing.Color.Transparent;
+            this.pnlLoginUP.Controls.Add(this.chkDahuy_Toathuoc);
+            this.pnlLoginUP.Controls.Add(this.txt_Password);
+            this.pnlLoginUP.Controls.Add(this.txt_UserName);
+            this.pnlLoginUP.Location = new System.Drawing.Point(-3, 198);
+            this.pnlLoginUP.Name = "pnlLoginUP";
+            this.pnlLoginUP.Size = new System.Drawing.Size(410, 159);
+            this.pnlLoginUP.TabIndex = 38;
             // 
-            // btn_Login
+            // chkDahuy_Toathuoc
             // 
-            this.btn_Login.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(1)))), ((int)(((byte)(204)))));
-            this.btn_Login.Appearance.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btn_Login.Appearance.Options.UseBackColor = true;
-            this.btn_Login.Appearance.Options.UseFont = true;
-            this.btn_Login.Location = new System.Drawing.Point(36, 374);
-            this.btn_Login.Name = "btn_Login";
-            this.btn_Login.Size = new System.Drawing.Size(314, 43);
-            this.btn_Login.TabIndex = 3;
-            this.btn_Login.Text = "Login";
+            this.chkDahuy_Toathuoc.Location = new System.Drawing.Point(39, 121);
+            this.chkDahuy_Toathuoc.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.chkDahuy_Toathuoc.Name = "chkDahuy_Toathuoc";
+            this.chkDahuy_Toathuoc.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkDahuy_Toathuoc.Properties.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.chkDahuy_Toathuoc.Properties.Appearance.Options.UseFont = true;
+            this.chkDahuy_Toathuoc.Properties.Appearance.Options.UseForeColor = true;
+            this.chkDahuy_Toathuoc.Properties.Caption = "Remeber me";
+            this.chkDahuy_Toathuoc.Size = new System.Drawing.Size(151, 22);
+            this.chkDahuy_Toathuoc.TabIndex = 37;
+            this.chkDahuy_Toathuoc.TabStop = false;
             // 
             // txt_Password
             // 
             this.txt_Password.EditValue = "Password...";
-            this.txt_Password.Location = new System.Drawing.Point(36, 272);
+            this.txt_Password.Location = new System.Drawing.Point(39, 75);
             this.txt_Password.Name = "txt_Password";
             this.txt_Password.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
             this.txt_Password.Properties.Appearance.BorderColor = System.Drawing.Color.Silver;
@@ -250,11 +252,14 @@
             this.txt_Password.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
             this.txt_Password.Size = new System.Drawing.Size(314, 30);
             this.txt_Password.TabIndex = 2;
+            this.txt_Password.Click += new System.EventHandler(this.txt_Password_Click);
+            this.txt_Password.Paint += new System.Windows.Forms.PaintEventHandler(this.txt_Password_Paint);
+            this.txt_Password.Leave += new System.EventHandler(this.txt_Password_Leave);
             // 
             // txt_UserName
             // 
             this.txt_UserName.EditValue = "Username...";
-            this.txt_UserName.Location = new System.Drawing.Point(36, 228);
+            this.txt_UserName.Location = new System.Drawing.Point(39, 29);
             this.txt_UserName.Name = "txt_UserName";
             this.txt_UserName.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
             this.txt_UserName.Properties.Appearance.BorderColor = System.Drawing.Color.Silver;
@@ -271,6 +276,40 @@
             this.txt_UserName.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
             this.txt_UserName.Size = new System.Drawing.Size(314, 30);
             this.txt_UserName.TabIndex = 1;
+            this.txt_UserName.Click += new System.EventHandler(this.txt_UserName_Click);
+            this.txt_UserName.Paint += new System.Windows.Forms.PaintEventHandler(this.txt_UserName_Paint);
+            this.txt_UserName.Leave += new System.EventHandler(this.txt_UserName_Leave);
+            // 
+            // lbl_Welcome
+            // 
+            this.lbl_Welcome.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Welcome.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(112)))), ((int)(((byte)(148)))));
+            this.lbl_Welcome.Appearance.Options.UseFont = true;
+            this.lbl_Welcome.Appearance.Options.UseForeColor = true;
+            this.lbl_Welcome.Location = new System.Drawing.Point(37, 177);
+            this.lbl_Welcome.Name = "lbl_Welcome";
+            this.lbl_Welcome.Size = new System.Drawing.Size(256, 15);
+            this.lbl_Welcome.TabIndex = 7;
+            this.lbl_Welcome.Text = "Welcome to DENTAL CARE  login to take action!";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(1)))), ((int)(((byte)(204)))));
+            this.btn_Login.Appearance.BackColor2 = System.Drawing.Color.Transparent;
+            this.btn_Login.Appearance.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Login.Appearance.Options.UseBackColor = true;
+            this.btn_Login.Appearance.Options.UseFont = true;
+            this.btn_Login.AppearanceHovered.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Login.AppearanceHovered.BackColor2 = System.Drawing.Color.Transparent;
+            this.btn_Login.AppearanceHovered.Options.UseBackColor = true;
+            this.btn_Login.AppearancePressed.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Login.AppearancePressed.Options.UseBackColor = true;
+            this.btn_Login.Location = new System.Drawing.Point(36, 374);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(314, 43);
+            this.btn_Login.TabIndex = 3;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_Login_MouseDown);
             // 
             // lbl_TitleLogin
             // 
@@ -285,18 +324,6 @@
             this.lbl_TitleLogin.Size = new System.Drawing.Size(84, 43);
             this.lbl_TitleLogin.TabIndex = 0;
             this.lbl_TitleLogin.Text = "Login";
-            // 
-            // checkEdit1
-            // 
-            this.checkEdit1.Location = new System.Drawing.Point(36, 323);
-            this.checkEdit1.Name = "checkEdit1";
-            this.checkEdit1.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.checkEdit1.Properties.Appearance.ForeColor = System.Drawing.Color.Black;
-            this.checkEdit1.Properties.Appearance.Options.UseFont = true;
-            this.checkEdit1.Properties.Appearance.Options.UseForeColor = true;
-            this.checkEdit1.Properties.Caption = "Remember me";
-            this.checkEdit1.Size = new System.Drawing.Size(115, 22);
-            this.checkEdit1.TabIndex = 8;
             // 
             // frmLogin
             // 
@@ -332,9 +359,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pnlLogin)).EndInit();
             this.pnlLogin.ResumeLayout(false);
             this.pnlLogin.PerformLayout();
+            this.pnlLoginUP.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chkDahuy_Toathuoc.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txt_Password.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txt_UserName.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -355,6 +383,7 @@
         private DevExpress.XtraEditors.TextEdit txt_UserName;
         private DevExpress.XtraEditors.LabelControl lbl_TitleLogin;
         private DevExpress.XtraEditors.LabelControl lbl_Welcome;
-        private DevExpress.XtraEditors.CheckEdit checkEdit1;
+        private DevExpress.XtraEditors.CheckEdit chkDahuy_Toathuoc;
+        private Panel pnlLoginUP;
     }
 }
--- dental-form/UI/Forms/frmLogin.cs
+++ dental-form/UI/Forms/frmLogin.cs
@@ -8,11 +8,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using System.Drawing;
+using Pen = System.Drawing.Pen;
+using Color = System.Drawing.Color;
+using System.Security.RightsManagement;
 
 namespace dental_form.UI.Forms
 {
     public partial class frmLogin : DevExpress.XtraEditors.XtraForm
     {
+        // variable change border color
+        bool focusU = false;
+        bool focusP= false;
+
         public frmLogin()
         {
             InitializeComponent();
@@ -20,7 +29,111 @@ namespace dental_form.UI.Forms
 
         private void picClose_EditValueChanged(object sender, EventArgs e)
         {
-            Application.Exit();
+            this.Close();
         }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txt_UserName_Click(object sender, EventArgs e)
+        {
+            focusU = true;
+            this.Refresh();
+            if (txt_UserName.Text=="Username...")
+            {
+                txt_UserName.Text = "";
+            }
+        }
+
+        private void txt_UserName_Leave(object sender, EventArgs e)
+        {
+            focusU = false;
+            this.Refresh();
+            if (txt_UserName.Text=="")
+            {
+                txt_UserName.Text = "Username...";
+            }
+          
+        }
+
+        private void txt_Password_Click(object sender, EventArgs e)
+        {
+            focusP = true;
+            this.Refresh();
+            if (txt_Password.Text=="Password...")
+            {
+                txt_Password.Text = "";
+                txt_Password.Properties.PasswordChar = '●';
+            }
+        }
+
+        private void txt_Password_Leave(object sender, EventArgs e)
+        {
+            focusP = false;
+            this.Refresh();
+            if (txt_Password.Text=="")
+            {
+                txt_Password.Text = "Password...";
+                txt_Password.Properties.PasswordChar = '\0' ;
+            }
+        }
+
+        // ---- start change border color when focus -----
+        private void txt_UserName_Paint(object sender, PaintEventArgs e)
+        {
+            if (focusU)
+            {
+                RectangleF rec = e.Graphics.ClipBounds;
+                rec.Inflate(-1, -1);
+                e.Graphics.DrawRectangle(Pens.Red,
+                    rec.Left,
+                    rec.Top,
+                    rec.Width,
+                    rec.Height);
+            }
+            else
+            {
+                RectangleF rec = e.Graphics.ClipBounds;
+                rec.Inflate(-1, -1);
+                e.Graphics.DrawRectangle(Pens.DimGray,
+                    rec.Left,
+                    rec.Top,
+                    rec.Width,
+                    rec.Height);
+            }
+        }
+
+        private void txt_Password_Paint(object sender, PaintEventArgs e)
+        {
+            if (focusP)
+            {
+                RectangleF rec = e.Graphics.ClipBounds;
+                rec.Inflate(-1, -1);
+                e.Graphics.DrawRectangle(Pens.Red,
+                    rec.Left,
+                    rec.Top,
+                    rec.Width,
+                    rec.Height);
+            }
+            else
+            {
+
+                RectangleF rec = e.Graphics.ClipBounds;
+                rec.Inflate(-1, -1);
+                e.Graphics.DrawRectangle(Pens.DimGray,
+                    rec.Left,
+                    rec.Top,
+                    rec.Width,
+                    rec.Height);
+            }
+        }
+
+        private void btn_Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            btn_Login.BackColor= Color.FromArgb(1, 129, 201);
+        }
+        // ---- end change border color when focus -----
     }
 }
